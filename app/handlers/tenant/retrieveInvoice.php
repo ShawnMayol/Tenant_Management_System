60006fb442bfb12e593bb3dd1749885099b38066
Alias314 <83777973+Alias314@users.noreply.github.com>
2024-07-11 06:32:41 +0800

    updated tenants page

--- app/handlers/tenant/retrieveInvoice.php
+++ app/handlers/tenant/retrieveInvoice.php
@@ -6,15 +6,17 @@ $loggedInUserID = $_SESSION['user_id'] ?? null;
 if (!$loggedInUserID) {
     die("Session user ID not found.");
 }
-echo $loggedInUserId;
 
-
-// Query to fetch invoices
-$sql = "SELECT i.invoice_ID, f.rent, f.tax, f.maintenance, f.totalAmount, i.dueDate
+// Query to fetch invoices with associated apartment details based on user ID
+$sql = "SELECT  i.invoice_ID, f.rent, f.tax, f.maintenance, f.totalAmount, i.dueDate, l.apartmentNumber, a.apartmentAddress, 
+                t.phoneNumber, t.emailAddress, t.firstName, t.middleName, t.lastName, ROUND(((f.rent + f.maintenance) * 0.05), 2) AS legitTax, i.dateIssued
         FROM invoice i
         JOIN fees f ON i.fee_ID = f.fee_ID
         JOIN lease l ON f.lease_ID = l.lease_ID
-        WHERE l.tenant_ID = $loggedInUserID
+        JOIN apartment a ON l.apartmentNumber = a.apartmentNumber
+        JOIN tenant t ON l.tenant_ID = t.tenant_ID
+        JOIN user u ON t.tenant_ID = u.tenant_ID
+        WHERE u.user_ID = $loggedInUserID
         ORDER BY i.dueDate DESC";
 
 $result = $conn->query($sql);
