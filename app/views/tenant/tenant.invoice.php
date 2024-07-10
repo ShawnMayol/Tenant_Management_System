f00c5fbd6f028b154df4f7b3020a3786cdfd8007
Alias314 <83777973+Alias314@users.noreply.github.com>
2024-07-11 07:48:30 +0800

    updated tenant page

--- app/views/tenant/tenant.invoice.php
+++ app/views/tenant/tenant.invoice.php
@@ -128,8 +128,7 @@
                 </div>
             </div>
             <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                <!-- Additional buttons if necessary -->
+              
             </div>
         </div>
     </div>
